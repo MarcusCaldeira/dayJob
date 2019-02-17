dca7c5d7f5cc94d0cdc7e69168e0ab4b2525148f
Marcus Caldeira <mac.caldr@gmail.com>
2019-02-17 10:34:33 -0600

    ok well where does this even go?

--- processorder.php
+++ processorder.php
@@ -3,6 +3,7 @@
 $tireqty = $_POST['tireqty'];
 $oilqty = $_POST['oilqty'];
 $sparkqty = $_POST['sparkqty'];
+$notes = $_POST['notes'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,31 +14,89 @@ $sparkqty = $_POST['sparkqty'];
 <h1>Bob's Auto Parts</h1>
 <h2>Order Results</h2>
 <?php
-echo "<p>Order processed at ".date('H:i, jS F Y')."</p>";
+
+echo "<p>Order processed at ";
+echo date('H:i, jS F Y');
+echo "</p>";
 
 echo '<p>Your order is as follows: </p>';
 echo htmlspecialchars($tireqty).' tires<br />';
 echo htmlspecialchars($oilqty).' bottles of oil<br />';
 echo htmlspecialchars($sparkqty).' spark plugs<br />';
+echo "Customer notes: ".htmlspecialchars($notes).'<br />';
+echo "How did you find Bob's: ";
+$find = htmlspecialchars($_POST['find']);
+switch ($find) {
+    case "a":
+        echo "regular customer";
+        break;
+    case "b":
+        echo "TV advert";
+        break;
+    case "c":
+        echo "phone directory";
+        break;
+    case "d":
+        echo "word of mouth";
+        break;
+    default:
+        echo 'source unknown';
+        break;
+}
+echo '<br/>';
+
+//3 part 3
+echo '<br/>';
+echo '<p>The type of each form field is:</p>';
+echo 'tireqty: ' . gettype($tireqty) . '<br />';
+echo 'oilqty: ' . gettype($oilqty) . '<br />';
+echo 'sparkqty: ' . gettype($sparkqty) . '<br />';
+echo 'notes: ' . gettype($notes) . '<br />';
+
+//4 cast quantities to integers
+$tireqty = (int)$tireqty;
+$oilqty = (int)$oilqty;
+$sparkqty = (int)$sparkqty;
+
+echo '<br/>';
+echo '<p>The type of each variable after converting tireqty, oilqty, and sparkqty to an int:</p>';
+echo 'tireqty: ' . gettype($tireqty) . '<br />';
+echo 'oilqty: ' . gettype($oilqty) . '<br />';
+echo 'sparkqty: ' . gettype($sparkqty) . '<br />';
+echo 'notes: ' . gettype($notes) . '<br />';
+
+echo '<br />';
 
 $totalqty = 0;
 $totalqty = $tireqty + $oilqty + $sparkqty;
-echo "<p>Items ordered: ".$totalqty."<br />";
-$totalamount = 0.00;
 
-define('TIREPRICE', 100);
-define('OILPRICE', 10);
-define('SPARKPRICE', 4);
+if ($tireqty < 0) {
+    echo "Error: tire quantity cannot be less than zero.";
+} elseif ($oilqty < 0) {
+    echo "Error: oil quantity cannot be less than zero.";
+} elseif ($sparkqty <0) {
+    echo "Error: spark quantity cannot be less than zero.";
+} elseif ($totalqty <=0) {
+    echo "Error: no items were entered in your order.";
+}else{
+    echo "<p>Items ordered: " . $totalqty . "<br />";
+    $totalamount = 0.00;
 
-$totalamount = $tireqty * TIREPRICE
-    + $oilqty * OILPRICE
-    + $sparkqty * SPARKPRICE;
+    define('TIREPRICE', 100);
+    define('OILPRICE', 10);
+    define('SPARKPRICE', 4);
 
-echo "Subtotal: $".number_format($totalamount,2)."<br />";
+    $totalamount = $tireqty * TIREPRICE
+        + $oilqty * OILPRICE
+        + $sparkqty * SPARKPRICE;
 
-$taxrate = 0.10;  // local sales tax is 10%
-$totalamount = $totalamount * (1 + $taxrate);
-echo "Total including tax: $".number_format($totalamount,2)."</p>";
+    echo "Subtotal: $" . number_format($totalamount, 2) . "<br />";
+
+    $taxrate = 0.10;  // local sales tax is 10%
+    $totalamount = $totalamount * (1 + $taxrate);
+    echo "Total including tax: $" . number_format($totalamount, 2) . "</p>";
+}
+
 
 ?>
 </body>
--- testPhp.php
+++ testPhp.php
@@ -4,7 +4,7 @@
     <title>Bob's Auto Parts - Order Form</title>
 </head>
 <body>
-<form action="processorder.php" method="post">
+<form action="processorder_v4.php" method="post">
     <table style="border: 0px;">
         <tr style="background: #cccccc;">
             <td style="width: 150px; text-align: center;">Item</td>
@@ -21,6 +21,22 @@
         <tr>
             <td>Spark Plugs</td>
             <td><input type="text" name="sparkqty" size="3" maxlength="3" /></td>
+        </tr>
+        <tr>
+            <td>How did you find Bob's?</td>
+            <td><select name="find">
+                    <option value = "a">I'm a regular customer</option>
+                    <option value = "b">TV advertising</option>
+                    <option value = "c">Phone directory</option>
+                    <option value = "d">Word of mouth</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>Customer notes:</td>
+            <td>
+                <input type="" name="notes" maxlength="200"/>
+            </td>
         </tr>
         <tr>
             <td colspan="2" style="text-align: center;"><input type="submit" value="Submit Order" /></td>
